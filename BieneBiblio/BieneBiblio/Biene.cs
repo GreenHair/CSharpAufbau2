b30fcc684cf4829a4e13ee6cc14562d4cea34aab
Auke Swen <aukeswen2001@yahoo.com>
2018-01-29 10:09:49 +0100

    Erster Ansatz

---
+++ BieneBiblio/BieneBiblio/Biene.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BieneBiblio
+{
+    public class Biene
+    {
+        public enum EGeschlecht
+        {
+            Männlich, Weiblich
+        }
+
+        static string farbe = "Gelb-Schwarz-Gestreift";
+
+        public int Alter { get; private set; }
+        public double Gewicht { get; private set; }
+        public EGeschlecht Geschlecht { get; private set; }
+
+        public Biene(int _alter, double _gewicht, EGeschlecht _was)
+        {
+            Alter = _alter;
+            Gewicht = _gewicht;
+            Geschlecht = _was;
+        }
+
+        public void Geburtstag()
+        {
+            Alter++;
+        }
+
+        public string Fliegen(double distanz)
+        {
+            double aenderung = distanz / 100;
+            if(Gewicht-aenderung > 0)
+            {
+                Gewicht -= aenderung;
+                return "geschafft";
+            }
+            else
+            {
+                return "Geht nicht";
+            }
+        }
+
+        public string Fressen(double menge)
+        {
+            Gewicht += menge * .5;
+            return "Ich bin soooo voll";
+        }
+
+        public string Farbe
+        {
+            get
+            {
+                return farbe;
+            }
+        }
+    }
+}
---
+++ BieneBiblio/KonsolenBiene/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BieneBiblio;
+
+namespace KonsolenBiene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Biene maja = new Biene(2, 1.2, Biene.EGeschlecht.Männlich);
+            Blume rose = new Blume();
+        }
+    }
+}
