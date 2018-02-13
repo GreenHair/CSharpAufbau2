ce1f1d4225c271c88f9c96dfed2817caab8914aa
Auke Swen <aukeswen2001@yahoo.com>
2018-02-13 15:42:28 +0100

    Erste WPF Datenbankanwendung

---
+++ PreparedStatements_DB/PreparedStatements_DB/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PreparedStatements_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "server=localhost;uid=root";
+            conn.Open();
+            conn.ChangeDatabase("test");
+
+            int zahl = 10;
+            string sql = "insert into prepstmt (zahl) values (" + zahl + ")";
+            Console.WriteLine("SQL: " + sql);
+            MySqlCommand comm = new MySqlCommand(sql, conn);
+            Console.WriteLine(comm.ExecuteNonQuery());
+
+            double komma = 3.1465;
+            sql = "insert into prepstmt (komma) values (" + komma + ")"; // Wurde ein exception audslösen
+            Console.WriteLine("SQL: " + sql);
+            string prepare = "insert into prepstmt (komma) values (@komma)";
+            comm = new MySqlCommand(prepare, conn);
+            MySqlParameter param = new MySqlParameter("@komma", MySqlDbType.Double, 0);
+            param.Value = komma;
+            comm.Parameters.Add(param);
+            comm.Prepare();
+            Console.WriteLine(comm.ExecuteNonQuery());
+
+            string wort = "Hallo Welt";
+            sql = "insert into prepstmt (wort) values ('" + wort + "')";
+            Console.WriteLine("SQL: " + sql);
+            comm = new MySqlCommand(sql, conn);
+            Console.WriteLine(comm.ExecuteNonQuery());
+
+            //SQL-Injection
+            wort = "';select * from prepstmt;";
+            sql = "insert into prepstmt (wort) values ('" + wort + "')";
+            Console.WriteLine("SQL: " + sql);
+
+            prepare = "insert into prepstmt (wort) values (@wort)";
+            comm = new MySqlCommand(prepare, conn);
+            param = new MySqlParameter("@wort", MySqlDbType.VarChar, 100);
+            param.Value = wort;
+            comm.Parameters.Add(param);
+            comm.Prepare();
+            Console.WriteLine(comm.ExecuteNonQuery());
+        }
+    }
+}
---
+++ Sprachqualifikation_DB/Sprachqualifikation_WPF/AllePersonen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SprachqualiLib;
+
+namespace Sprachqualifikation_WPF
+{
+    /// <summary>
+    /// Interaction logic for AllePersonen.xaml
+    /// </summary>
+    public partial class AllePersonen : Window
+    {
+        Person[] Personen;
+        int index;
+
+        public AllePersonen()
+        {
+            InitializeComponent();
+            Personen = new MySqlAbfragen("sprachquali", "root", "").AllePersonen();
+
+            txtNummer.Text = Personen[0].Pers_nr;
+            txtName.Text = Personen[0].Nachname;
+            txtVorname.Text = Personen[0].Vorname;
+        }
+
+        private void btnVor_Click(object sender, RoutedEventArgs e)
+        {
+            if(index < Personen.Length - 1)
+            {
+                index++;
+                txtNummer.Text = Personen[index].Pers_nr;
+                txtName.Text = Personen[index].Nachname;
+                txtVorname.Text = Personen[index].Vorname;
+            }
+        }
+
+        private void btnzurück_Click(object sender, RoutedEventArgs e)
+        {
+            if (index > 0)
+            {
+                index--;
+                txtNummer.Text = Personen[index].Pers_nr;
+                txtName.Text = Personen[index].Nachname;
+                txtVorname.Text = Personen[index].Vorname;
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
